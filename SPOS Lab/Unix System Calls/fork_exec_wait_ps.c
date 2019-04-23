dddaf53131d123b175a49d9aaa99fd61dc5a5e53
sidpandagle <siddhant.pandagle1998@gmail.com>
2019-04-24 00:10:08 +0530

    Add files via upload

---
+++ SPOS Lab/Unix System Calls/fork_exec_wait_ps.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char * argv[]){
+                                                                                                                                   
+  char * ls_args[3] = { "ps", "-l", NULL} ;
+  pid_t c_pid, pid;
+  int status;
+
+  c_pid = fork();
+
+  if (c_pid == 0){
+    /* CHILD */
+
+    printf("Child: executing ls\n");
+
+    //execute ps                                                                                                                                                               
+    execvp( ls_args[0], ls_args);
+    //only get here if exec failed                                                                                                                                             
+    perror("execve failed");
+  }else if (c_pid > 0){
+    /* PARENT */
+
+    if( (pid = wait(&status)) < 0){
+      perror("wait");
+      _exit(1);
+    }
+
+    printf("Parent: finished\n");
+
+  }else{
+    perror("fork failed");
+    _exit(1);
+  }
+
+  return 0; //return success
+}
+
